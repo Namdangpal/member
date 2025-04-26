06e5d96269afba89d8d8baa87c41085d30b16b9a
dangpal <dangpal10@gmail.com>
2025-04-26 22:24:37 +0900

    클래서 명: Address.java, Delivery.java 변경

--- src/main/java/com/ilsungprint/itteam/member/domain/Address.java
+++ src/main/java/com/ilsungprint/itteam/member/domain/Address.java
@@ -1,5 +1,43 @@
 package com.ilsungprint.itteam.member.domain;
 
 public class Address {
-    
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    // getter 및 setter를 추가해주세요
+    public String getStreet() {
+        return street;
+        }
+
+        public void setStreet(String street) {
+        this.street = street;
+        }
+
+        public String getCity() {
+        return city;
+        }
+
+        public void setCity(String city) {
+        this.city = city;
+        }
+
+        public String getState() {
+        return state;
+        }
+
+        public void setState(String state) {
+        this.state = state;
+        }
+
+        public String getZipCode() {
+        return zipCode;
+        }
+
+        public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+        }
 }
+    
+    
--- src/main/java/com/ilsungprint/itteam/member/domain/Delivery.java
+++ src/main/java/com/ilsungprint/itteam/member/domain/Delivery.java
@@ -1,5 +1,42 @@
 package com.ilsungprint.itteam.member.domain;
 
 public class Delivery {
-    
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    // Constructors, getters, and setters can be added here
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+  
 }
